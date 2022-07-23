cd991b414048b798c068ad063ac10f5f00a092d6
monsalex23 <monsalex23@gmail.com>
2022-07-23 10:31:55 -0300

    Criando um Banco Digital com Java e Orientação a Objetos

---
+++ src/Conta.java
@@ -0,0 +1,43 @@
+public abstract class Conta implements Iconta{
+    private static int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+    
+    protected int agencia;
+    protected  int numero;
+    protected  double saldo;
+
+
+    public Conta() {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+
+    @Override
+    public void sacar(double valor) {
+
+    }
+
+    @Override
+    public void depositar(double valor) {
+
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+
+}
